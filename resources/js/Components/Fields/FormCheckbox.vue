23d890472e8fca3bab4b7c4b2546acfcc962acf3
Justin Navarro <info@justinnavarro.net>
2024-09-01 10:24:46 -0400

    created checkbox component

---
+++ resources/js/Components/Fields/FormCheckbox.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import type { RulesType } from '@/types/form';
+import useForm from '@/Composables/useForm';
+import Check from "@/Components/Icons/outline/Check.vue";
+import FormLabel from "@/Components/Fields/FormLabel.vue";
+import FormButton from "@/Components/Fields/FormButton.vue";
+
+const props = defineProps<{
+    label: string;
+    defaultChecked?: boolean;
+    rules?: RulesType | Array<keyof RulesType>;
+    validateOnInit?: boolean;
+}>();
+
+const emit = defineEmits<{ (e: 'handleUpdate', checked: boolean): void }>();
+
+const { error, labelId, getInputValue, updateInputValue } = useForm({
+    label: props.label,
+    validateOnInit: props.validateOnInit,
+    value: props.defaultChecked ? 'checked' : '',
+    rules: props.rules,
+});
+
+const updateValue = () => {
+    const value = getInputValue.value === 'checked' ? '' : 'checked';
+    updateInputValue(value);
+    emit('handleUpdate', value === 'checked');
+};
+</script>
+
+<template>
+    <div class="flex flex-row items-center">
+        <FormButton checkbox @click="updateValue" v-if="getInputValue !== 'checked'">
+            <Check classes="size-4 text-white dark:text-gray-700" />
+        </FormButton>
+
+        <FormButton checkbox color="primary" @click="updateValue" v-if="getInputValue === 'checked'">
+            <Check classes="size-4" />
+        </FormButton>
+
+        <div class="ml-2">
+            <FormLabel :error="error" :label="label" :labelId="labelId" />
+        </div>
+    </div>
+</template>
