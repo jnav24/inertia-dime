cb03845ffd9f5a7fbfb4e7c6745e9997a4b286b2
Justin Navarro <info@justinnavarro.net>
2024-09-03 19:23:13 -0400

    update the register page

--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
 import GuestLayout from '@/Layouts/GuestLayout.vue';
-import InputError from '@/Components/InputError.vue';
-import InputLabel from '@/Components/InputLabel.vue';
-import PrimaryButton from '@/Components/PrimaryButton.vue';
-import TextInput from '@/Components/TextInput.vue';
-import { Head, Link, useForm } from '@inertiajs/vue3';
+import BudgetForm from "@/Components/Fields/BudgetForm.vue";
+import FormInput from "@/Components/Fields/FormInput.vue";
+import FormButton from "@/Components/Fields/FormButton.vue";
+import { Head, useForm } from '@inertiajs/vue3';
+import Typography from "@/Components/Elements/Typography.vue";
+import AppLink from "@/Components/Elements/AppLink.vue";
 
 const form = useForm({
     name: '',
@@ -20,86 +21,30 @@ const submit = () => {
         },
     });
 };
+
 </script>
 
 <template>
     <GuestLayout>
         <Head title="Register" />
 
-        <form @submit.prevent="submit">
-            <div>
-                <InputLabel for="name" value="Name" />
+        <div class="my-12">
+            <Typography variant="h1">Create an Account</Typography>
+        </div>
 
-                <TextInput
-                    id="name"
-                    type="text"
-                    class="mt-1 block w-full"
-                    v-model="form.name"
-                    required
-                    autofocus
-                    autocomplete="name"
-                />
-
-                <InputError class="mt-2" :message="form.errors.name" />
+        <BudgetForm>
+            <div class="mb-6 space-y-6">
+                <FormInput label="Name" :rules="['required', 'min:3', 'max:255']" />
+                <FormInput label="Email" :rules="['required', 'email']" />
+                <FormInput label="Password" :rules="['required', 'min:12', 'upper', 'symbol', 'lower', 'has-int']" password />
+                <FormInput label="Confirm Password" :rules="['required', 'match:password']" password />
             </div>
 
-            <div class="mt-4">
-                <InputLabel for="email" value="Email" />
+            <FormButton block color="secondary" submit>Register</FormButton>
+        </BudgetForm>
 
-                <TextInput
-                    id="email"
-                    type="email"
-                    class="mt-1 block w-full"
-                    v-model="form.email"
-                    required
-                    autocomplete="username"
-                />
-
-                <InputError class="mt-2" :message="form.errors.email" />
-            </div>
-
-            <div class="mt-4">
-                <InputLabel for="password" value="Password" />
-
-                <TextInput
-                    id="password"
-                    type="password"
-                    class="mt-1 block w-full"
-                    v-model="form.password"
-                    required
-                    autocomplete="new-password"
-                />
-
-                <InputError class="mt-2" :message="form.errors.password" />
-            </div>
-
-            <div class="mt-4">
-                <InputLabel for="password_confirmation" value="Confirm Password" />
-
-                <TextInput
-                    id="password_confirmation"
-                    type="password"
-                    class="mt-1 block w-full"
-                    v-model="form.password_confirmation"
-                    required
-                    autocomplete="new-password"
-                />
-
-                <InputError class="mt-2" :message="form.errors.password_confirmation" />
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                <Link
-                    :href="route('login')"
-                    class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
-                >
-                    Already registered?
-                </Link>
-
-                <PrimaryButton class="ms-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                    Register
-                </PrimaryButton>
-            </div>
-        </form>
+        <div class="my-6 text-center">
+            <Typography variant="caption">Already have an account? <AppLink :href="route('login')">Log in</AppLink></Typography>
+        </div>
     </GuestLayout>
 </template>
