be597b7a3b22dc922c44f641fa3aa13304720353
Justin Navarro <info@justinnavarro.net>
2024-09-05 11:12:21 -0400

    update the forgot password page

--- resources/js/Pages/Auth/ForgotPassword.vue
+++ resources/js/Pages/Auth/ForgotPassword.vue
@@ -1,59 +1,34 @@
 <script setup lang="ts">
 import GuestLayout from '@/Layouts/GuestLayout.vue';
-import InputError from '@/Components/InputError.vue';
-import InputLabel from '@/Components/InputLabel.vue';
-import PrimaryButton from '@/Components/PrimaryButton.vue';
-import TextInput from '@/Components/TextInput.vue';
-import { Head, useForm } from '@inertiajs/vue3';
+import { Head } from '@inertiajs/vue3';
+import Typography from "@/Components/Elements/Typography.vue";
+import BudgetForm from "@/Components/Fields/BudgetForm.vue";
+import FormInput from "@/Components/Fields/FormInput.vue";
+import FormButton from "@/Components/Fields/FormButton.vue";
 
 defineProps<{
     status?: string;
 }>();
-
-const form = useForm({
-    email: '',
-});
-
-const submit = () => {
-    form.post(route('password.email'));
-};
 </script>
 
 <template>
     <GuestLayout>
         <Head title="Forgot Password" />
 
-        <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
-            Forgot your password? No problem. Just let us know your email address and we will email you a password reset
-            link that will allow you to choose a new one.
-        </div>
-
         <div v-if="status" class="mb-4 font-medium text-sm text-green-600 dark:text-green-400">
             {{ status }}
         </div>
 
-        <form @submit.prevent="submit">
-            <div>
-                <InputLabel for="email" value="Email" />
+        <div class="space-y-2 my-12">
+            <Typography variant="h1">Forgot Password</Typography>
+            <Typography variant="body1">No worries! Enter your email and we will send you reset instructions.</Typography>
+        </div>
 
-                <TextInput
-                    id="email"
-                    type="email"
-                    class="mt-1 block w-full"
-                    v-model="form.email"
-                    required
-                    autofocus
-                    autocomplete="username"
-                />
-
-                <InputError class="mt-2" :message="form.errors.email" />
+        <BudgetForm :action="route('password.email')">
+            <div class="space-y-4">
+                <FormInput label="Email" :rules="['required', 'email']"/>
+                <FormButton color="primary" block submit>Reset Password</FormButton>
             </div>
-
-            <div class="flex items-center justify-end mt-4">
-                <PrimaryButton :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                    Email Password Reset Link
-                </PrimaryButton>
-            </div>
-        </form>
+        </BudgetForm>
     </GuestLayout>
 </template>
