1f1c1d2515dac73601f0d5832f644ec8af6a67d1
Justin Navarro <info@justinnavarro.net>
2024-09-06 18:26:03 -0400

    update the reset password page

--- resources/js/Pages/Auth/ResetPassword.vue
+++ resources/js/Pages/Auth/ResetPassword.vue
@@ -1,88 +1,33 @@
 <script setup lang="ts">
 import GuestLayout from '@/Layouts/GuestLayout.vue';
-import InputError from '@/Components/InputError.vue';
-import InputLabel from '@/Components/InputLabel.vue';
-import PrimaryButton from '@/Components/PrimaryButton.vue';
-import TextInput from '@/Components/TextInput.vue';
-import { Head, useForm } from '@inertiajs/vue3';
+import { Head } from '@inertiajs/vue3';
+import FormButton from "@/Components/Fields/FormButton.vue";
+import FormInput from "@/Components/Fields/FormInput.vue";
+import Typography from "@/Components/Elements/Typography.vue";
+import BudgetForm from "@/Components/Fields/BudgetForm.vue";
+import { validation } from '@/utils/helpers';
 
-const props = defineProps<{
+defineProps<{
     email: string;
     token: string;
 }>();
-
-const form = useForm({
-    token: props.token,
-    email: props.email,
-    password: '',
-    password_confirmation: '',
-});
-
-const submit = () => {
-    form.post(route('password.store'), {
-        onFinish: () => {
-            form.reset('password', 'password_confirmation');
-        },
-    });
-};
 </script>
 
 <template>
     <GuestLayout>
         <Head title="Reset Password" />
 
-        <form @submit.prevent="submit">
-            <div>
-                <InputLabel for="email" value="Email" />
+        <div class="space-y-2 my-12">
+            <Typography variant="h1">Reset Password</Typography>
+        </div>
 
-                <TextInput
-                    id="email"
-                    type="email"
-                    class="mt-1 block w-full"
-                    v-model="form.email"
-                    required
-                    autofocus
-                    autocomplete="username"
-                />
-
-                <InputError class="mt-2" :message="form.errors.email" />
+        <BudgetForm :action="route('password.email')">
+            <div class="space-y-4">
+                <FormInput label="Email" :rules="['required', 'email']" :value="email" />
+                <FormInput label="Password" :rules="validation.password" password />
+                <FormInput label="Confirm Password" :rules="['required', 'match:password']" password />
+                <FormButton color="primary" block submit>Reset Password</FormButton>
             </div>
-
-            <div class="mt-4">
-                <InputLabel for="password" value="Password" />
-
-                <TextInput
-                    id="password"
-                    type="password"
-                    class="mt-1 block w-full"
-                    v-model="form.password"
-                    required
-                    autocomplete="new-password"
-                />
-
-                <InputError class="mt-2" :message="form.errors.password" />
-            </div>
-
-            <div class="mt-4">
-                <InputLabel for="password_confirmation" value="Confirm Password" />
-
-                <TextInput
-                    id="password_confirmation"
-                    type="password"
-                    class="mt-1 block w-full"
-                    v-model="form.password_confirmation"
-                    required
-                    autocomplete="new-password"
-                />
-
-                <InputError class="mt-2" :message="form.errors.password_confirmation" />
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                <PrimaryButton :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                    Reset Password
-                </PrimaryButton>
-            </div>
-        </form>
+        </BudgetForm>
     </GuestLayout>
 </template>
