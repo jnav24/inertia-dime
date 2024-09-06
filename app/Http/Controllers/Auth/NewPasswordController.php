701a2cb4a46b4569ee1e3b0e8adb05d72896e428
Justin Navarro <info@justinnavarro.net>
2024-09-06 18:10:54 -0400

    update the password rules in resetting a password

--- app/Http/Controllers/Auth/NewPasswordController.php
+++ app/Http/Controllers/Auth/NewPasswordController.php
@@ -37,7 +37,15 @@ class NewPasswordController extends Controller
         $request->validate([
             'token' => 'required',
             'email' => 'required|email',
-            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'password' => [
+                'required',
+                'confirmed',
+                Rules\Password::min(12)
+                    ->mixedCase()
+                    ->numbers()
+                    ->symbols()
+                    ->uncompromised()
+            ],
         ]);
 
         // Here we will attempt to reset the user's password. If it is successful we
