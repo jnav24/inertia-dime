1c396693807e6aae8831a618775d540ec4402ce5
Justin Navarro <info@justinnavarro.net>
2024-09-06 19:56:24 -0400

    update verify email page

--- resources/js/Pages/Auth/VerifyEmail.vue
+++ resources/js/Pages/Auth/VerifyEmail.vue
@@ -1,18 +1,16 @@
 <script setup lang="ts">
 import { computed } from 'vue';
 import GuestLayout from '@/Layouts/GuestLayout.vue';
-import PrimaryButton from '@/Components/PrimaryButton.vue';
-import { Head, Link, useForm } from '@inertiajs/vue3';
+import { Head } from '@inertiajs/vue3';
+import Typography from "@/Components/Elements/Typography.vue";
+import BudgetForm from "@/Components/Fields/BudgetForm.vue";
+import FormButton from "@/Components/Fields/FormButton.vue";
+import AppLink from "@/Components/Elements/AppLink.vue";
+import Alert from "@/Components/Elements/Alert.vue";
 
 const props = defineProps<{
     status?: string;
 }>();
-
-const form = useForm({});
-
-const submit = () => {
-    form.post(route('verification.send'));
-};
 
 const verificationLinkSent = computed(() => props.status === 'verification-link-sent');
 </script>
@@ -21,29 +19,31 @@ const verificationLinkSent = computed(() => props.status === 'verification-link-
     <GuestLayout>
         <Head title="Email Verification" />
 
-        <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
-            Thanks for signing up! Before getting started, could you verify your email address by clicking on the link
-            we just emailed to you? If you didn't receive the email, we will gladly send you another.
+        <div class="space-y-4 my-8">
+            <Typography variant="h1">Verify your email</Typography>
+            <Typography variant="body1">Thanks for signing up! Before getting started, could you verify your email address by clicking on the link
+                we just emailed to you? If you didn't receive the email, we will gladly send you another.</Typography>
         </div>
 
-        <div class="mb-4 font-medium text-sm text-green-600 dark:text-green-400" v-if="verificationLinkSent">
-            A new verification link has been sent to the email address you provided during registration.
-        </div>
+        <Alert
+            v-if="verificationLinkSent"
+            message="A new verification link has been sent to the email address you provided during registration."
+            type="success"
+            title="Success"
+        />
 
-        <form @submit.prevent="submit">
-            <div class="mt-4 flex items-center justify-between">
-                <PrimaryButton :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                    Resend Verification Email
-                </PrimaryButton>
+        <BudgetForm :action="route('verification.send')">
+            <FormButton color="primary" submit block>Resend Verification Email</FormButton>
+        </BudgetForm>
 
-                <Link
+        <div class="mt-6 text-center">
+            <Typography variant="caption">
+                <AppLink
                     :href="route('logout')"
                     method="post"
                     as="button"
-                    class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
-                    >Log Out</Link
-                >
-            </div>
-        </form>
+                >Log Out</AppLink>
+            </Typography>
+        </div>
     </GuestLayout>
 </template>
