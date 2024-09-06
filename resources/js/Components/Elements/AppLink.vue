274b9a534b4a319a79cc4b348f0ff216830ef18c
Justin Navarro <info@justinnavarro.net>
2024-09-06 19:44:47 -0400

    update applink to dynamically pass all props to link

--- resources/js/Components/Elements/AppLink.vue
+++ resources/js/Components/Elements/AppLink.vue
@@ -5,5 +5,5 @@ const props = defineProps<InertiaLinkProps>()
 </script>
 
 <template>
-    <Link class="text-primary hover:text-dm-primary" :href="props.href"><slot /></Link>
+    <Link class="text-primary hover:text-dm-primary" v-bind="props"><slot /></Link>
 </template>
