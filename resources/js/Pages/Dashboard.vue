22c73b8d03bc68e8951e0ef24137b43fe124b694
Justin Navarro <info@justinnavarro.net>
2024-09-13 07:07:02 -0400

    add AuthenticatedContentLayout.vue component and updated the dashboard page

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import AuthenticatedContentLayout from '@/Layouts/AuthenticatedContentLayout.vue';
 import { Head } from '@inertiajs/vue3';
 </script>
 
@@ -7,18 +8,12 @@ import { Head } from '@inertiajs/vue3';
     <Head title="Dashboard" />
 
     <AuthenticatedLayout>
-        <template #header>
-            <h2 class="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
-                Dashboard
-            </h2>
-        </template>
+        <template #header> Dashboard </template>
 
-        <div class="py-12">
-            <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-                <div class="overflow-hidden bg-white shadow-sm dark:bg-gray-800 sm:rounded-lg">
-                    <div class="p-6 text-gray-900 dark:text-gray-100">You're logged in!</div>
-                </div>
+        <AuthenticatedContentLayout>
+            <div class="overflow-hidden bg-white shadow-sm dark:bg-gray-800 sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">You're logged in!</div>
             </div>
-        </div>
+        </AuthenticatedContentLayout>
     </AuthenticatedLayout>
 </template>
