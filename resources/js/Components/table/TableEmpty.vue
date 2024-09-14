c77565117931ad702b429be2536c3fe8c86a8a39
Justin Navarro <info@justinnavarro.net>
2024-09-14 09:12:27 -0400

    create table empty component

---
+++ resources/js/Components/table/TableEmpty.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import Typography from '@/Components/Elements/Typography.vue';
+import type { ConcreteComponent } from 'vue';
+import FormButton from '@/Components/Fields/FormButton.vue';
+
+type Props = {
+    button?: {
+        label: string;
+        icon?: ConcreteComponent;
+    };
+    content?: string;
+    title?: string;
+};
+
+withDefaults(defineProps<Props>(), {
+    title: 'No Results',
+    content: 'Could not find any matching results. Please try again',
+});
+defineEmits<{ (e: 'onclick'): void }>();
+</script>
+
+<template>
+    <section
+        class="border-lm-stroke bg-lm-secondary dark:border-dm-stroke flex w-full flex-col items-center rounded-lg border py-20 dark:bg-dm-secondary"
+    >
+        <div class="w-72 space-y-4 text-center">
+            <Typography variant="h2">{{ title }}</Typography>
+            <Typography variant="body1">
+                <span class="text-gray-500 dark:text-gray-400">{{ content }}</span>
+            </Typography>
+
+            <FormButton v-if="button?.label" :icon="button?.icon" @click="$emit('onclick')">
+                {{ button.label }}
+            </FormButton>
+        </div>
+    </section>
+</template>
