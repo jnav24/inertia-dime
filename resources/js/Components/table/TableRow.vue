be890eb1a24ef3fcbe4c5df8a20478c900b81cce
Justin Navarro <info@justinnavarro.net>
2024-09-14 09:12:10 -0400

    created table row component

---
+++ resources/js/Components/table/TableRow.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+
+<template>
+    <div
+        :class="[
+            'border-lm-stroke dark:border-dm-stroke flex flex-row items-center space-x-4 border-0 px-4 py-6 shadow-sm',
+            'bg-lm-secondary hover:bg-gray-100 dark:bg-dm-secondary dark:hover:bg-dm-primary/25',
+        ]"
+    >
+        <slot />
+    </div>
+</template>
