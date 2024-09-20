5b294961590b45b0dd99d113c0a5ffa9d9ec146f
Justin Navarro <info@justinnavarro.net>
2024-09-20 01:40:25 -0400

    created expense type model

---
+++ app/Models/ExpenseType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use App\Enums\CacheEnum;
+use App\Traits\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Cache;
+
+class ExpenseType extends Model
+{
+    use HasFactory, HasUuids;
+
+    protected $fillable = [
+        'uuid',
+        'name',
+        'slug',
+        'type',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'uuid' => 'string',
+        ];
+    }
+
+    public static function cached()
+    {
+        return Cache::get(CacheEnum::EXPENSE_TYPES->value) ??
+            Cache::remember(CacheEnum::EXPENSE_TYPES->value, now()->addDay(), fn () => static::get());
+    }
+
+    public static function grouped()
+    {
+        return static::cached()
+            ->groupBy(fn ($item) => $item->type);
+    }
+}
