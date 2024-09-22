9ad77817acf884f87fb76a60e3cacf327bf4b684
Justin Navarro <info@justinnavarro.net>
2024-09-22 10:46:55 -0400

    update the expense type and expense trait

--- app/Enums/ExpenseTypeEnum.php
+++ app/Enums/ExpenseTypeEnum.php
@@ -23,11 +23,22 @@ enum ExpenseTypeEnum: string
     case SUBSCRIPTION = 'subscription';
     case TAX = 'tax';
     case TRAVEL = 'travel';
-    case UTILITY = 'utilities';
+    case UTILITY = 'utility';
     case VEHICLE = 'vehicle';
 
-    public function allTypes()
+    /**
+     * @return array<string>
+     */
+    public static function allTypes(): array
     {
         return array_map(fn ($item) => Str::title($item->value), ExpenseTypeEnum::cases());
+    }
+
+    /**
+     * @return array<string>
+     */
+    public static function allRelationships(): array
+    {
+        return array_map(fn ($item) => Str::camel(Str::plural($item->value)), ExpenseTypeEnum::cases());
     }
 }
