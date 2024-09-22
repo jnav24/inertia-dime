aaae33270451857d53ba6a56f37506d486fa14e1
Justin Navarro <info@justinnavarro.net>
2024-09-21 20:37:23 -0400

    created utility

---
+++ app/Models/UtilityTemplate.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use App\Casts\EncryptedExpenseSpend;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class UtilityTemplate extends Model
+{
+    use SoftDeletes, HasFactory;
+
+    protected $fillable = [
+        'uuid',
+        'data',
+        'budget_template_id',
+        'expense_type_id',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'data' => EncryptedExpenseSpend::class,
+            'uuid' => 'string',
+        ];
+    }
+
+    public function budgetTemplate(): BelongsTo
+    {
+        return $this->belongsTo(BudgetTemplate::class);
+    }
+
+    public function expenseType(): BelongsTo
+    {
+        return $this->belongsTo(related: ExpenseType::class, ownerKey: 'uuid');
+    }
+}
