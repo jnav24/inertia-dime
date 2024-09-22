d098837fd8ede821eb39d4ba6423ee0a26033058
Justin Navarro <info@justinnavarro.net>
2024-09-21 20:32:14 -0400

    created personal

---
+++ database/migrations/2024_09_21_160704_create_personal_templates_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::create('personal_templates', function (Blueprint $table) {
+            $table->id();
+            $table->uuid()->unique();
+            $table->text('data');
+            $table->foreignId('budget_template_id')->constrained()->cascadeOnDelete();
+            $table->foreignUuid('expense_type_id')->constrained(table: 'expense_types', column: 'uuid')->cascadeOnDelete();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('personal_templates');
+    }
+};
