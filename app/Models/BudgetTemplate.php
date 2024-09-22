a4ff8875f77891b99e34e2b6d1a786864fd60a10
Justin Navarro <info@justinnavarro.net>
2024-09-22 10:43:19 -0400

    add relationships to the budget template model

--- app/Models/BudgetTemplate.php
+++ app/Models/BudgetTemplate.php
@@ -7,6 +7,7 @@ use App\Traits\WithExpenses;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class BudgetTemplate extends Model
@@ -28,5 +29,105 @@ class BudgetTemplate extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function banks(): HasMany
+    {
+        return $this->hasMany(BankTemplate::class);
+    }
+
+    public function childcares(): HasMany
+    {
+        return $this->hasMany(ChildcareTemplate::class);
+    }
+
+    public function creditCards(): HasMany
+    {
+        return $this->hasMany(CreditCardTemplate::class);
+    }
+
+    public function education(): HasMany
+    {
+        return $this->hasMany(EducationTemplate::class);
+    }
+
+    public function entertainments(): HasMany
+    {
+        return $this->hasMany(EntertainmentTemplate::class);
+    }
+
+    public function food(): HasMany
+    {
+        return $this->hasMany(FoodTemplate::class);
+    }
+
+    public function gifts(): HasMany
+    {
+        return $this->hasMany(GiftTemplate::class);
+    }
+
+    public function housings(): HasMany
+    {
+        return $this->hasMany(HousingTemplate::class);
+    }
+
+    public function incomes(): HasMany
+    {
+        return $this->hasMany(IncomeTemplate::class);
+    }
+
+    public function investments(): HasMany
+    {
+        return $this->hasMany(InvestmentTemplate::class);
+    }
+
+    public function loans(): HasMany
+    {
+        return $this->hasMany(LoanTemplate::class);
+    }
+
+    public function medicals(): HasMany
+    {
+        return $this->hasMany(MedicalTemplate::class);
+    }
+
+    public function miscellaneouses(): HasMany
+    {
+        return $this->hasMany(MiscellaneousTemplate::class);
+    }
+
+    public function personals(): HasMany
+    {
+        return $this->hasMany(PersonalTemplate::class);
+    }
+
+    public function shoppings(): HasMany
+    {
+        return $this->hasMany(ShoppingTemplate::class);
+    }
+
+    public function subscriptions(): HasMany
+    {
+        return $this->hasMany(SubscriptionTemplate::class);
+    }
+
+    public function taxes(): HasMany
+    {
+        return $this->hasMany(TaxTemplate::class);
+    }
+
+    public function travel(): HasMany
+    {
+        return $this->hasMany(TravelTemplate::class);
+    }
+
+    public function utilities(): HasMany
+    {
+        return $this->hasMany(UtilityTemplate::class);
+    }
+
+    public function vehicles(): HasMany
+    {
+        return $this->hasMany(VehicleTemplate::class);
     }
 }
