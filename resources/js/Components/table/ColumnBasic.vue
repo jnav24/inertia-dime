71fe87a6568f648cc766e33fc4b00987deb6d68c
Justin Navarro <info@justinnavarro.net>
2024-09-23 11:46:17 -0400

    created ColumnBasic component

---
+++ resources/js/Components/table/ColumnBasic.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import Typography from '@/Components/Elements/Typography.vue';
+
+defineProps<{ value: string }>();
+</script>
+
+<template>
+    <Typography variant="body2">{{ value }}</Typography>
+</template>
