efc6a61cdfc70dbb0723cc5cd5b68f4487ce05bd
Justin Navarro <info@justinnavarro.net>
2024-09-28 00:20:23 -0400

    emit close modal and populate the form fields in the gain expense

--- resources/js/Components/Forms/GainExpenseForm.vue
+++ resources/js/Components/Forms/GainExpenseForm.vue
@@ -1,24 +1,29 @@
 <script setup lang="ts">
+import { computed } from 'vue';
+import { convertToDollar } from '@/utils/functions';
 import FormInput from '@/Components/Fields/FormInput.vue';
 import FormSelect from '@/Components/Fields/FormSelect.vue';
 import ExpenseFormActions from '@/Components/Forms/ExpenseFormActions.vue';
 import { ExpenseFormEmits, GainExpenseFormProps } from '@/types/expenses';
 
 defineEmits<ExpenseFormEmits>();
-defineProps<GainExpenseFormProps>();
+const props = defineProps<GainExpenseFormProps>();
+const amount = computed(() => convertToDollar(props.expense?.data.amount));
 </script>
 
 <template>
     <div class="mb-6 grid grid-cols-2 gap-4">
         <FormInput label="Name" :rules="['required', 'min:3']" :value="expense?.data.name" />
-        <FormInput label="Amount" :rules="['required', 'float:2']" :value="expense?.data.amount" />
+        <FormInput label="Amount" :rules="['required', 'float:2']" :value="amount" />
         <FormSelect
             :items="types"
             label="Account Type"
+            item-label="name"
+            item-value="id"
             :value="expense?.expense_type_id"
             :rules="['required']"
         />
     </div>
 
-    <ExpenseFormActions />
+    <ExpenseFormActions @close="$emit('close')" />
 </template>
