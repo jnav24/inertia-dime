9e97151530db684e101b742d7fb558b1f92c0d51
Justin Navarro <info@justinnavarro.net>
2024-10-02 07:06:18 -0400

    add datepicker to confirmation component

--- resources/js/Components/Forms/ExpenseFormConfirmation.vue
+++ resources/js/Components/Forms/ExpenseFormConfirmation.vue
@@ -2,6 +2,7 @@
 import FormInput from '@/Components/Fields/FormInput.vue';
 import FormCheckbox from '@/Components/Fields/FormCheckbox.vue';
 import FormTextarea from '@/Components/Fields/FormTextarea.vue';
+import FormDatePicker from '@/Components/Fields/FormDatePicker.vue';
 </script>
 
 <template>
@@ -10,7 +11,7 @@ import FormTextarea from '@/Components/Fields/FormTextarea.vue';
 
         <div class="mt-4 grid grid-cols-2 gap-4">
             <FormInput label="Confirmation" />
-            <FormInput label="Confirmation" />
+            <FormDatePicker label="Paid Date" />
         </div>
 
         <FormTextarea label="Notes" />
