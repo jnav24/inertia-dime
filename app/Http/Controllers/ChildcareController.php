2ec9ea7dba603efc210225824b47ebf1f1de1c75
Justin Navarro <info@justinnavarro.net>
2024-10-02 07:10:22 -0400

    initial build of the childcare controller

---
+++ app/Http/Controllers/ChildcareController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Data\ExpenseSpendDto;
+use App\Http\Requests\CommonExpenseRequest;
+use App\Models\ChildcareTemplate;
+
+class ChildcareController extends Controller
+{
+    public function store(CommonExpenseRequest $request)
+    {
+        $validated = $request->validated();
+
+        if ($validated['template']) {
+            ChildcareTemplate::create([
+                'data' => new ExpenseSpendDto(
+                    name: $validated['name'],
+                    amount: $validated['amount'],
+                    due_date: $validated['due_date'],
+                ),
+                'expense_type_id' => $validated['account_type'],
+                'budget_template_id' => auth()->user()->budgetTemplate->id,
+            ]);
+
+            return redirect()->route('budget.template.index');
+        }
+
+        return redirect()->back();
+    }
+
+    public function update(CommonExpenseRequest $request, string $uuid)
+    {
+        $validated = $request->validated();
+
+        if ($validated['template']) {
+            $template = ChildcareTemplate::where('uuid', $uuid)->first();
+            $template->update([
+                'data' => new ExpenseSpendDto(
+                    name: $validated['name'],
+                    amount: $validated['amount'],
+                    due_date: $validated['due_date'],
+                ),
+                'expense_type_id' => $validated['account_type'],
+            ]);
+
+            return redirect()->route('budget.template.index');
+        }
+
+        return redirect()->back();
+    }
+
+    public function destroy(string $uuid)
+    {
+        $template = ChildcareTemplate::where('uuid', $uuid)->first();
+
+        if (empty($template)) {
+            // $template = Childcare::where('uuid', $uuid)->first();
+        }
+
+        $template?->delete();
+
+        return redirect()->back();
+    }
+}
