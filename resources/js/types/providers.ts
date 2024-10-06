8d362edb5e58cd264d4a49b97ea052240fc1c9b4
Justin Navarro <info@justinnavarro.net>
2024-10-05 21:04:21 -0400

    created page props type

--- resources/js/types/providers.ts
+++ resources/js/types/providers.ts
@@ -9,3 +9,16 @@ export type NotificationProps = {
     message: string;
     type: 'success' | 'warn' | 'error' | 'info';
 };
+
+export type PageProps = {
+    auth: { id: number; name: string; email: string; email_verified_at: string };
+    errors: Record<string, string>;
+    flash: { message: string | null };
+    ziggy: {
+        url: string;
+        defaults: any[];
+        location: string;
+        port: number;
+        routes: Record<string, { methods: string[]; uri: string }>;
+    };
+};
