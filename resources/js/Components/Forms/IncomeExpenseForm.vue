494ff78df665b0635cd7ccc68e94f787a5780ab6
Justin Navarro <info@justinnavarro.net>
2024-10-09 22:33:48 -0400

    update the income expense form to save and update properly

--- resources/js/Components/Forms/IncomeExpenseForm.vue
+++ resources/js/Components/Forms/IncomeExpenseForm.vue
@@ -2,14 +2,31 @@
 import FormInput from '@/Components/Fields/FormInput.vue';
 import ExpenseFormActions from '@/Components/Forms/ExpenseFormActions.vue';
 import FormSelect from '@/Components/Fields/FormSelect.vue';
+import { ExpenseFormEmits, GainExpenseFormProps } from '@/types/expenses';
+import { computed } from 'vue';
+import { convertToDollar } from '@/utils/functions';
+import FormDatePicker from '@/Components/Fields/FormDatePicker.vue';
+import { getDefaultDate } from '@/utils/helpers';
+
+defineEmits<ExpenseFormEmits>();
+const props = defineProps<GainExpenseFormProps>();
+const amount = computed(() => convertToDollar(props.expense?.data.amount));
 </script>
 
 <template>
     <div class="mb-6 grid grid-cols-2 gap-4">
-        <FormInput label="Name" :rules="['required', 'min:3']" />
-        <FormInput label="Amount" :rules="['required', 'float:2']" />
-        <FormSelect :items="[]" label="Account Type" value="" :rules="['required']" />
-        <FormInput label="Pay Date" :rules="['required', 'float:2']" />
+        <FormInput label="Template" hidden :value="!!isTemplate" />
+        <FormInput label="Name" :rules="['required', 'min:3']" :value="expense?.data.name" />
+        <FormInput label="Amount" :rules="['required', 'float:2']" :value="amount" />
+        <FormSelect
+            :items="types"
+            label="Account Type"
+            item-label="name"
+            item-value="id"
+            :value="expense?.expense.id"
+            :rules="['required']"
+        />
+        <FormDatePicker label="Pay Date" :value="getDefaultDate(expense?.data.pay_date)" />
     </div>
 
     <ExpenseFormActions @close="$emit('close')" />
