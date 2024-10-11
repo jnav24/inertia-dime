35e2cce28c544ff647ecbd2ab6b963fc50367439
Justin Navarro <info@justinnavarro.net>
2024-10-11 07:34:40 -0400

    created column badge component

---
+++ resources/js/Components/table/ColumnBadge.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import Typography from '@/Components/Elements/Typography.vue';
+
+defineProps<{ value: string }>();
+</script>
+
+<template>
+    <div class="inline-block rounded-lg border border-gray-400 bg-gray-100 p-2">
+        <Typography variant="caption">
+            <span class="text-gray-500">{{ value }}</span>
+        </Typography>
+    </div>
+</template>
