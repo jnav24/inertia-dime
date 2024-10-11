4e125e64c12509e0bef5d893d9299b3d32e7dda8
Justin Navarro <info@justinnavarro.net>
2024-10-10 20:20:02 -0400

    add common and credit card forms

--- resources/js/Components/Forms/CommonExpenseForm.vue
+++ resources/js/Components/Forms/CommonExpenseForm.vue
@@ -1,18 +1,39 @@
 <script setup lang="ts">
+import { computed } from 'vue';
+import { dueDates } from '@/utils/helpers';
+import { convertToDollar } from '@/utils/functions';
 import FormInput from '@/Components/Fields/FormInput.vue';
 import ExpenseFormActions from '@/Components/Forms/ExpenseFormActions.vue';
 import FormSelect from '@/Components/Fields/FormSelect.vue';
 import ExpenseFormConfirmation from '@/Components/Forms/ExpenseFormConfirmation.vue';
+import { ExpenseFormEmits, CommonExpenseFormProps } from '@/types/expenses';
+
+defineEmits<ExpenseFormEmits>();
+const props = defineProps<CommonExpenseFormProps>();
+const amount = computed(() => convertToDollar(props.expense?.data.amount));
 </script>
 
 <template>
     <div class="mb-6 grid grid-cols-2 gap-4">
-        <FormInput label="Name" :rules="['required', 'min:3']" />
-        <FormInput label="Amount" :rules="['required', 'float:2']" />
-        <FormSelect :items="[]" label="Account Type" value="" :rules="['required']" />
-        <FormSelect :items="[]" label="Due Date" value="" />
+        <FormInput label="Template" hidden :value="!!isTemplate" />
+        <FormInput label="Name" :rules="['required', 'min:3']" :value="expense?.data.name" />
+        <FormInput label="Amount" :rules="['required', 'float:2']" :value="amount" />
+        <FormSelect
+            :items="types"
+            label="Account Type"
+            item-label="name"
+            item-value="id"
+            :rules="['required']"
+            :value="expense?.expense.id"
+        />
+        <FormSelect
+            v-if="isTemplate"
+            :items="dueDates"
+            label="Due Date"
+            :value="expense?.data.due_date ?? 1"
+        />
     </div>
 
-    <ExpenseFormConfirmation />
+    <ExpenseFormConfirmation v-if="!isTemplate" :expense="expense" />
     <ExpenseFormActions @close="$emit('close')" />
 </template>
--- resources/js/Components/Forms/CreditCardExpenseForm.vue
+++ resources/js/Components/Forms/CreditCardExpenseForm.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { computed } from 'vue';
 import { convertToDollar } from '@/utils/functions';
+import { dueDates } from '@/utils/helpers';
 import { ExpenseFormEmits, GainExpenseFormProps } from '@/types/expenses';
 import FormInput from '@/Components/Fields/FormInput.vue';
 import ExpenseFormActions from '@/Components/Forms/ExpenseFormActions.vue';
@@ -10,25 +11,43 @@ import ExpenseFormConfirmation from '@/Components/Forms/ExpenseFormConfirmation.
 defineEmits<ExpenseFormEmits>();
 const props = defineProps<GainExpenseFormProps>();
 const amount = computed(() => convertToDollar(props.expense?.data.amount));
+const balance = computed(() => convertToDollar(props.expense?.data.balance));
+const limit = computed(() => convertToDollar(props.expense?.data.limit));
 </script>
 
 <template>
     <div class="mb-6 grid grid-cols-2 gap-4">
-        <FormInput label="Name" :rules="['required', 'min:3']" />
-        <FormInput label="Amount" :rules="['required', 'float:2']" />
-        <FormSelect :items="[]" label="Account Type" value="" :rules="['required']" />
-        <FormInput label="Account Balance" :rules="['required', 'float:2']" />
-        <FormSelect :items="[]" label="Due Date" value="" />
+        <FormInput label="Template" hidden :value="!!isTemplate" />
+        <FormInput label="Name" :rules="['required', 'min:3']" :value="expense?.data.name" />
+        <FormInput label="Amount" :rules="['required', 'float:2']" :value="amount" />
+        <FormSelect
+            :items="types"
+            label="Account Type"
+            item-label="name"
+            item-value="id"
+            :value="expense?.expense.id"
+            :rules="['required']"
+        />
+        <FormInput label="Balance" :rules="['required', 'float:2']" :value="balance" />
+        <FormSelect
+            v-if="isTemplate"
+            :items="dueDates"
+            label="Due Date"
+            :value="expense?.data.due_date ?? 1"
+        />
     </div>
 
     <div class="mt-4 grid grid-cols-2 gap-4">
-        <FormInput label="Credit Limit" />
-        <FormInput label="APR" />
-        <FormInput label="Last 4 Numbers" />
-        <FormInput label="Expiration Month" />
-        <FormInput label="Expiration Year" />
+        <FormInput label="Limit" :value="limit" />
+        <FormInput label="APR" :value="expense?.data.apr" />
     </div>
 
-    <ExpenseFormConfirmation />
+    <div class="mt-4 grid grid-cols-3 gap-4">
+        <FormInput label="Last 4" :value="expense?.data.last_4" />
+        <FormInput label="Exp Month" :value="expense?.data.exp_month" />
+        <FormInput label="Exp Year" :value="expense?.data.exp_year" />
+    </div>
+
+    <ExpenseFormConfirmation v-if="!isTemplate" :expense="expense" />
     <ExpenseFormActions @close="$emit('close')" />
 </template>
