cb3caca028c1f3cfcb54c6c07c3c127e93adfb73
Justin Navarro <info@justinnavarro.net>
2024-10-13 18:43:55 -0400

    sort profile components in the tabs

---
+++ resources/js/Components/Elements/Card.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+    <div class="bg-white p-4 shadow dark:bg-gray-800 sm:rounded-lg sm:p-8">
+        <slot />
+    </div>
+</template>
--- resources/js/Pages/Profile/Edit.vue
+++ resources/js/Pages/Profile/Edit.vue
@@ -4,6 +4,10 @@ import DeleteUserForm from './Partials/DeleteUserForm.vue';
 import UpdatePasswordForm from './Partials/UpdatePasswordForm.vue';
 import UpdateProfileInformationForm from './Partials/UpdateProfileInformationForm.vue';
 import { Head } from '@inertiajs/vue3';
+import AuthenticatedContentLayout from '@/Layouts/AuthenticatedContentLayout.vue';
+import Tabs from '@/Components/tabs/Tabs.vue';
+import TabItem from '@/Components/tabs/TabItem.vue';
+import Card from '@/Components/Elements/Card.vue';
 
 defineProps<{
     mustVerifyEmail?: boolean;
@@ -15,30 +19,38 @@ defineProps<{
     <Head title="Profile" />
 
     <AuthenticatedLayout>
-        <template #header>
-            <h2 class="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
-                Profile
-            </h2>
-        </template>
+        <template #header> Profile </template>
 
-        <div class="py-12">
-            <div class="mx-auto max-w-7xl space-y-6 sm:px-6 lg:px-8">
-                <div class="bg-white p-4 shadow dark:bg-gray-800 sm:rounded-lg sm:p-8">
-                    <UpdateProfileInformationForm
-                        :must-verify-email="mustVerifyEmail"
-                        :status="status"
-                        class="max-w-xl"
-                    />
-                </div>
+        <AuthenticatedContentLayout>
+            <Tabs>
+                <TabItem title="Overview">
+                    <section class="space-y-6">
+                        <Card>
+                            <UpdateProfileInformationForm
+                                :must-verify-email="mustVerifyEmail"
+                                :status="status"
+                                class="max-w-xl"
+                            />
+                        </Card>
 
-                <div class="bg-white p-4 shadow dark:bg-gray-800 sm:rounded-lg sm:p-8">
-                    <UpdatePasswordForm class="max-w-xl" />
-                </div>
+                        <Card>
+                            <DeleteUserForm class="max-w-xl" />
+                        </Card>
+                    </section>
+                </TabItem>
 
-                <div class="bg-white p-4 shadow dark:bg-gray-800 sm:rounded-lg sm:p-8">
-                    <DeleteUserForm class="max-w-xl" />
-                </div>
-            </div>
-        </div>
+                <TabItem title="Security">
+                    <section class="space-y-6">
+                        <Card>
+                            <UpdatePasswordForm class="max-w-xl" />
+                        </Card>
+
+                        <Card>
+                            <h2>MFA</h2>
+                        </Card>
+                    </section>
+                </TabItem>
+            </Tabs>
+        </AuthenticatedContentLayout>
     </AuthenticatedLayout>
 </template>
