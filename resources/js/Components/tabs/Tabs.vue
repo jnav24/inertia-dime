6a32a943f64952efdc554e758ec266d20c7c762e
Justin Navarro <info@justinnavarro.net>
2024-10-13 18:12:16 -0400

    refactor and add logic for the active tab

--- resources/js/Components/tabs/Tabs.vue
+++ resources/js/Components/tabs/Tabs.vue
@@ -1,20 +1,31 @@
 <script setup lang="ts">
-import { computed, provide, ref } from 'vue';
+import { computed, provide, reactive, ref } from 'vue';
 import Typography from '@/Components/Elements/Typography.vue';
 import { TabContext, TabContextType } from '@/types/tabs';
 
-const tabs = ref([]);
+const tabs = reactive<Record<string, HTMLButtonElement | null>>({});
 const selectedTab = ref('');
 
 const activeTab = computed(() => selectedTab.value);
+const tabKeys = computed(() => Object.keys(tabs));
+const tabIndicatorStyles = computed(() => {
+    const rect = tabs[activeTab.value]?.getBoundingClientRect();
+    let leftPosition = 0;
+
+    if (tabKeys.value.length) {
+        leftPosition = rect?.left - tabs[tabKeys.value[0]]?.getBoundingClientRect()?.left;
+    }
+
+    return { left: `${leftPosition}px`, width: `${rect?.width ?? 0}px` };
+});
 
 const addTab = (tab: string) => {
-    if (!tabs.value.includes(tab)) {
-        if (!tabs.value.length) {
+    if (!tabs[tab]) {
+        if (!tabKeys.value.length) {
             selectedTab.value = tab;
         }
 
-        tabs.value = [...tabs.value, tab];
+        tabs[tab] = null;
     }
 };
 
@@ -26,15 +37,28 @@ provide<TabContextType>(TabContext, {
 
 <template>
     <section>
-        <div class="flex space-x-4 pb-6">
-            <button v-for="(tab, idx) in tabs" :key="idx" @click="selectedTab = tab" type="button">
-                <Typography variant="body2">
-                    {{ tab }}
-                </Typography>
-            </button>
+        <div class="relative">
+            <div class="flex space-x-4 pb-6">
+                <button
+                    v-for="(tab, idx) in tabKeys"
+                    :key="idx"
+                    @click="selectedTab = tab"
+                    :ref="(el) => (tabs[tab] = el)"
+                    type="button"
+                >
+                    <Typography variant="body2">
+                        {{ tab }}
+                    </Typography>
+                </button>
+            </div>
+
+            <div
+                class="absolute bottom-0 h-1 rounded bg-dm-primary transition-all duration-300 ease-in-out"
+                :style="tabIndicatorStyles"
+            ></div>
         </div>
 
-        <div class="bg-red-300">
+        <div class="mt-6 bg-red-300">
             <slot />
         </div>
     </section>
