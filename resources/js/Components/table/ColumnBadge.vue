7aff8ac9bffe52e6585762a532cd2cd55f6f8db0
Justin Navarro <info@justinnavarro.net>
2024-10-17 18:58:44 -0400

    add more colors to the column badge component

--- resources/js/Components/table/ColumnBadge.vue
+++ resources/js/Components/table/ColumnBadge.vue
@@ -1,13 +1,33 @@
 <script setup lang="ts">
 import Typography from '@/Components/Elements/Typography.vue';
 
-defineProps<{ value: string }>();
+type Props = {
+    color?: 'success' | 'danger' | 'default';
+    value: string;
+};
+
+withDefaults(defineProps<Props>(), { color: 'default' });
 </script>
 
 <template>
-    <div class="inline-block rounded-lg border border-gray-400 bg-gray-100 p-2">
+    <div
+        class="inline-block rounded-lg border p-2"
+        :class="{
+            'border-danger bg-danger/20': color === 'danger',
+            'border-success bg-success/20': color === 'success',
+            'border-gray-400 bg-gray-100': color === 'default',
+        }"
+    >
         <Typography variant="caption">
-            <span class="text-gray-500">{{ value }}</span>
+            <span
+                :class="{
+                    'text-danger': color === 'danger',
+                    'text-success': color === 'success',
+                    'text-gray-500': color === 'default',
+                }"
+            >
+                {{ value }}
+            </span>
         </Typography>
     </div>
 </template>
