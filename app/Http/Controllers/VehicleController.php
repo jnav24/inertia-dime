0c6c047168456b37019f8ce7a76c16f92705fc46
Justin Navarro <info@justinnavarro.net>
2024-10-18 18:55:21 -0400

    create controller and request to save the vehicle

--- app/Http/Controllers/VehicleController.php
+++ app/Http/Controllers/VehicleController.php
@@ -2,25 +2,57 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Data\VehicleDto;
+use App\Http\Requests\VehicleExpenseRequest;
+use App\Models\VehicleTemplate;
 
 class VehicleController extends Controller
 {
-    public function store(Request $request)
+    public function store(VehicleExpenseRequest $request)
     {
         $validated = $request->validated();
+        $model = $validated['template'] ? VehicleTemplate::class : null;
 
-        if ($validated['template']) {}
+        $model::create([
+            'data' => new VehicleDto(
+                amount: $validated['amount'],
+                balance: $validated['balance'],
+                due_date: $validated['due_date'],
+                mileage: $validated['mileage'],
+                confirmation: $validated['confirmation'] ?? null,
+                notes: $validated['notes'] ?? null,
+                paid_date: $validated['paid_date'] ?? null,
+            ),
+            // @todo for this and all others, may have to pass this id in the request
+            'budget_template_id' => auth()->user()->budgetTemplate->id,
+            'expense_type_id' => $validated['account_type'],
+            'user_vehicle_id' => $validated['vehicle'],
+        ]);
 
         return redirect()->back()
             ->with('message', 'Vehicle was created successfully');
     }
 
-    public function update(Request $request, string $uuid)
+    public function update(VehicleExpenseRequest $request, string $uuid)
     {
         $validated = $request->validated();
+        $model = $validated['template'] ? VehicleTemplate::class : null;
 
-        if ($validated['template']) {}
+        $model->update([
+            'data' => new VehicleDto(
+                amount: $validated['amount'],
+                balance: $validated['balance'],
+                due_date: $validated['due_date'],
+                mileage: $validated['mileage'],
+                confirmation: $validated['confirmation'],
+                notes: $validated['notes'],
+                paid_date: $validated['paid_date'],
+            ),
+            // @todo for this and all others, may have to pass this id in the request
+            'budget_template_id' => auth()->user()->budgetTemplate->id,
+            'expense_type_id' => $validated['account_type'],
+            'user_vehicle_id' => $validated['vehicle'],
+        ]);
 
         return redirect()->back()
             ->with('message', 'Vehicle was created successfully');
---
+++ app/Http/Requests/VehicleExpenseRequest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class VehicleExpenseRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'account_type' => 'required|uuid',
+            'amount' => 'required|numeric|decimal:2',
+            'balance' => 'nullable|decimal:2',
+            'confirmation' => 'nullable|string|max:255',
+            'due_date' => 'int|max:31',
+            'template' => 'bool',
+            'vehicle' => 'required|uuid',
+            'paid_date' => 'nullable|date',
+            'mileage' => 'nullable|string',
+            'notes' => 'nullable|string',
+        ];
+    }
+}
