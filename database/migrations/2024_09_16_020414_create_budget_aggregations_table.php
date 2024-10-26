0a02a70fcc40f75902c565d1d83b3e445958a04f
Justin Navarro <info@justinnavarro.net>
2024-10-25 20:25:48 -0400

    I added relation to user on the budget aggregation migration

--- database/migrations/2024_09_16_020414_create_budget_aggregations_table.php
+++ database/migrations/2024_09_16_020414_create_budget_aggregations_table.php
@@ -12,6 +12,7 @@ return new class extends Migration
             $table->id();
             $table->uuid()->unique();
             $table->text('data');
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
             $table->foreignId('budget_id')->constrained()->cascadeOnDelete();
             $table->timestamps();
             $table->softDeletes();
