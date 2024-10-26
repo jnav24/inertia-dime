3e3bf620ec231907d6f396d00f0ab663a6048b03
Justin Navarro <info@justinnavarro.net>
2024-10-25 20:03:16 -0400

    add support for button styles in the AppLink component

--- resources/js/Components/Elements/AppLink.vue
+++ resources/js/Components/Elements/AppLink.vue
@@ -1,9 +1,20 @@
 <script setup lang="ts">
 import { Link, InertiaLinkProps } from '@inertiajs/vue3';
+import { computed } from 'vue';
 
 const props = defineProps<InertiaLinkProps>();
+
+const styles = computed(() => {
+    if (props.as === 'button') {
+        const base =
+            'focus:shadow-outline rounded-md transition duration-150 focus:outline-none px-6 py-3 text-sm';
+        return `${base} border border-gray-300 bg-white text-gray-700 hover:bg-gray-100 active:bg-gray-200`;
+    }
+
+    return 'text-primary hover:text-dm-primary';
+});
 </script>
 
 <template>
-    <Link class="text-primary hover:text-dm-primary" v-bind="props"><slot /></Link>
+    <Link :class="styles" v-bind="props"><slot /></Link>
 </template>
