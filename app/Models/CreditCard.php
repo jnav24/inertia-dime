5623bdff388faae81dbceaf49291c6c521cfdc6b
Justin Navarro <info@justinnavarro.net>
2024-11-02 14:49:22 -0400

    created credit card model and migration

---
+++ app/Models/CreditCard.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use App\Casts\EncryptedCreditCard;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class CreditCard extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = [
+        'uuid',
+        'data',
+        'budget_id',
+        'expense_type_id',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'data' => EncryptedCreditCard::class,
+            'uuid' => 'string',
+        ];
+    }
+
+    public function budget(): BelongsTo
+    {
+        return $this->belongsTo(Budget::class);
+    }
+
+    public function expenseType(): BelongsTo
+    {
+        return $this->belongsTo(related: ExpenseType::class, ownerKey: 'uuid');
+    }
+}
