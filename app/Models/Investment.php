798bd51d1fec76c2c0ea6108da65d1d8a32bbae6
Justin Navarro <info@justinnavarro.net>
2024-11-02 13:43:00 -0400

    created investment migration and model

---
+++ app/Models/Investment.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Investment extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = [
+        'uuid',
+        'data',
+        'budget_id',
+        'expense_type_id',
+    ];
+
+    public function budget(): BelongsTo
+    {
+        return $this->belongsTo(Budget::class);
+    }
+
+    public function expenseType(): BelongsTo
+    {
+        return $this->belongsTo(ExpenseType::class);
+    }
+
+    protected function casts(): array
+    {
+        return [
+            'uuid' => 'string',
+        ];
+    }
+}
