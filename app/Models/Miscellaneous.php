483212392198d861b0e090c780ebbbd9659974e7
Justin Navarro <info@justinnavarro.net>
2024-11-04 09:50:00 -0500

    created miscellaneous migration and model

---
+++ app/Models/Miscellaneous.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use App\Casts\EncryptedExpenseSpend;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Miscellaneous extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = [
+        'uuid',
+        'data',
+        'budget_id',
+        'expense_type_id',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'data' => EncryptedExpenseSpend::class,
+            'uuid' => 'string',
+        ];
+    }
+
+    public function budget(): BelongsTo
+    {
+        return $this->belongsTo(Budget::class);
+    }
+
+    public function expenseType(): BelongsTo
+    {
+        return $this->belongsTo(related: ExpenseType::class, ownerKey: 'uuid');
+    }
+}
