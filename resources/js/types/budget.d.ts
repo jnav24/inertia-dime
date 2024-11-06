6ab2244006bea335c74f8ca41512e6e716eb3f1c
Justin Navarro <info@justinnavarro.net>
2024-11-06 15:53:07 -0500

    created expenses type

--- resources/js/types/budget.d.ts
+++ resources/js/types/budget.d.ts
@@ -1,3 +1,5 @@
+import { Expenses } from '@/types/expenses';
+
 enum BudgetAggregationEnum {
     EARNED = 'earned',
     SAVED = 'saved',
@@ -8,6 +10,7 @@ export type Budget = {
     id: uuid;
     name: string;
     budget_cycle: string;
+    expenses: Expenses;
 };
 
 export type BudgetAggregation = Record<
--- resources/js/types/expenses.d.ts
+++ resources/js/types/expenses.d.ts
@@ -13,71 +13,47 @@ export type ExpenseFormActionProps = {
     notes?: string;
 };
 
+export type Expenses = {
+    banks: GainExpense;
+    childcares: CommonExpense;
+    creditCards: CreditCardExpense;
+    education: CommonExpense;
+    entertainments: CommonExpense;
+    food: CommonExpense;
+    gifts: CommonExpense;
+    housings: CommonExpense;
+    incomes: IncomeExpense;
+    investments: CommonExpense;
+    loans: CommonExpense;
+    medicals: CommonExpense;
+    miscellaneouses: CommonExpense;
+    personals: CommonExpense;
+    shoppings: CommonExpense;
+    subscriptions: CommonExpense;
+    taxes: CommonExpense;
+    travel: CommonExpense;
+    utilities: CommonExpense;
+    vehicles: VehicleExpense;
+};
+
 export type GainExpenseFormProps = ExpenseFormProps & {
-    expense?: {
-        id: string;
-        data: {
-            name: string;
-            amount: number;
-        };
-        expense: ExpenseType;
-    };
+    expense?: GainExpense;
 };
 
 export type CommonExpenseFormProps = ExpenseFormProps & {
-    expense?: {
-        id: string;
-        data: ExpenseFormActionProps & {
-            name: string;
-            amount: number;
-            due_date?: number;
-        };
-        expense: ExpenseType;
-    };
+    expense?: CommonExpense;
 };
 
 export type CreditCardExpenseFormProps = ExpenseFormProps & {
-    expense?: {
-        id: string;
-        data: ExpenseFormActionProps & {
-            name: string;
-            amount: number;
-            balance: number;
-            limit: number;
-            apr: number;
-            last_4: number;
-            exp_month: number;
-            exp_year: number;
-            due_date?: number;
-        };
-        expense: ExpenseType;
-    };
+    expense?: CreditCardExpense;
 };
 
 export type IncomeExpenseFormProps = ExpenseFormProps & {
-    expense?: {
-        id: string;
-        data: {
-            name: string;
-            amount: number;
-            pay_date: string;
-        };
-        expense: ExpenseType;
-    };
+    expense?: IncomeExpense;
 };
 
 export type UserVehicleExpenseFormProps = ExpenseFormProps & {
-    expense?: {
-        id: string;
-        data: ExpenseFormActionProps & {
-            amount: number;
-            balance: number;
-            due_date?: number;
-            mileage: number;
-        };
-        expense: ExpenseType;
-        vehicle: UserVehicle;
-    };
+    expense?: VehicleExpense;
 };
 
 type UserVehicle = {
@@ -94,4 +70,61 @@ type ExpenseType = {
     id: string;
     name: string;
     slug: string;
+};
+
+type CreditCardExpense = {
+    id: string;
+    data: ExpenseFormActionProps & {
+        name: string;
+        amount: number;
+        balance: number;
+        limit: number;
+        apr: number;
+        last_4: number;
+        exp_month: number;
+        exp_year: number;
+        due_date?: number;
+    };
+    expense: ExpenseType;
+};
+
+type GainExpense = {
+    id: string;
+    data: {
+        name: string;
+        amount: number;
+    };
+    expense: ExpenseType;
+};
+
+type IncomeExpense = {
+    id: string;
+    data: {
+        name: string;
+        amount: number;
+        pay_date: string;
+    };
+    expense: ExpenseType;
+};
+
+type CommonExpense = {
+    id: string;
+    data: ExpenseFormActionProps & {
+        name: string;
+        amount: number;
+        due_date?: number;
+    };
+    expense: ExpenseType;
+};
+
+type VehicleExpense = {
+    id: string;
+    data: ExpenseFormActionProps & {
+        amount: number;
+        balance: number;
+        due_date?: number;
+        mileage: number;
+    };
+    expense: ExpenseType;
+    vehicle: UserVehicle;
 };
