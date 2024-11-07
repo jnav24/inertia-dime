78da284c05963569d17ac4a735aa785bd5dbbc3f
Justin Navarro <info@justinnavarro.net>
2024-11-06 19:52:16 -0500

    created column trend component

---
+++ resources/js/Components/table/ColumnTrend.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import TrendUp from '@/Components/Icons/outline/TrendUp.vue';
+import TrendDown from '@/Components/Icons/outline/TrendDown.vue';
+import Fire from '@/Components/Icons/outline/Fire.vue';
+import { ColumnTrendProps } from '@/types/table';
+import { computed } from 'vue';
+import { BudgetAggregationEnum } from '@/types/budget';
+
+const props = defineProps<ColumnTrendProps>();
+
+const saved = computed(() => {
+    return (
+        props.aggregation.data.find((item) => item.type === BudgetAggregationEnum.SAVED)?.value ?? 0
+    );
+});
+</script>
+
+<template>
+    <div class="flex">
+        <TrendDown v-if="saved < 0" classes="text-danger size-8" />
+        <TrendUp v-else classes="text-success size-8" />
+        <Fire v-if="saved === highestSaved" classes="text-orange-500 size-8" />
+    </div>
+</template>
