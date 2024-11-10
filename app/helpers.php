c336f593f888c9a5649909a0b1eaebbdb3209c4a
Justin Navarro <info@justinnavarro.net>
2024-11-10 14:11:04 -0500

    create function to extract the uuid from a string

--- app/helpers.php
+++ app/helpers.php
@@ -6,3 +6,15 @@ if (! function_exists('convertToCents')) {
         return intval(round(floatval($dollar) * 100));
     }
 }
+
+if (! function_exists('extractUuid')) {
+    function extractUuid(string $value): bool | string {
+        $pattern = '/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/i';
+
+        if (preg_match($pattern, $value, $matches)) {
+            return $matches[0];
+        }
+
+        return false;
+    }
+}
