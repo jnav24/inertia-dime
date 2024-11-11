8f42028c18d00f61ae6703c091bcf7f886cb4e28
Justin Navarro <info@justinnavarro.net>
2024-11-11 10:38:57 -0500

    started building out the budget show page

--- resources/js/Pages/BudgetShow.vue
+++ resources/js/Pages/BudgetShow.vue
@@ -3,21 +3,97 @@ import { Head } from '@inertiajs/vue3';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import AuthenticatedContentLayout from '@/Layouts/AuthenticatedContentLayout.vue';
 import { PageProps } from '@/types/providers';
+import { computed, onMounted, ref } from 'vue';
+import Sidebar from '@/Components/Elements/Sidebar.vue';
+import Plus from '@/Components/Icons/outline/Plus.vue';
+import { toTitleCase } from '@/utils/functions';
+import FormButton from '@/Components/Fields/FormButton.vue';
+import Table from '@/Components/table/Table.vue';
+import { columns } from '@/utils/helpers';
+import { formatDate } from '@/utils/timestamp';
+import ExpenseModal from '@/Components/modals/ExpenseModal.vue';
 
 type Props = PageProps & {
     budget: any;
+    types: any;
 };
 
-defineProps<Props>();
+const props = defineProps<Props>();
+
+const categories = ref<string[]>([]);
+const formData = ref(undefined);
+const showModal = ref(false);
+const selectedItem = ref('');
+
+const defaultCategory = computed(() => categories.value?.[0] ?? 'banks');
+
+const category = computed(() => {
+    const categories: Record<string, string> = {
+        creditCards: 'credit cards',
+    };
+
+    return categories[selectedItem.value] ?? selectedItem.value;
+});
+
+const handleColumnEvent = (e: { type: string; obj: any }) => {
+    switch (e.type) {
+        case 'edit':
+            formData.value = e.obj;
+            showModal.value = true;
+            break;
+        case 'delete':
+            console.log('open delete confirm');
+            break;
+    }
+};
+
+onMounted(() => {
+    categories.value = Object.keys(props.types);
+    selectedItem.value = categories.value?.[0] ?? 'banks';
+});
 </script>
 
 <template>
     <Head title="Budgets" />
 
     <AuthenticatedLayout>
+        <ExpenseModal
+            v-model:show="showModal"
+            :expense="category"
+            :form-data="formData"
+            :notify="flash.message ?? ''"
+            :types="types[selectedItem]?.data ?? []"
+        />
+
+        <template #header>{{ formatDate('MMM yyyy', budget.data.budget_cycle) }}</template>
+
         <AuthenticatedContentLayout>
-            <p>Hey Peeps</p>
-            <pre>{{ budget }}</pre>
+            <section class="grid grid-cols-4 gap-3">
+                <Sidebar
+                    :value="defaultCategory"
+                    :items="categories"
+                    @on-selection="selectedItem = $event"
+                />
+
+                <div class="col-span-4 ml-3 mr-4 sm:mx-0 md:col-span-3">
+                    <div class="mb-4 text-right">
+                        <FormButton color="primary" :icon="Plus" @click="showModal = true">
+                            Add {{ toTitleCase(selectedItem) }}
+                        </FormButton>
+                    </div>
+
+                    <Table
+                        :columns="[...(columns[selectedItem] ?? [])]"
+                        :empty="{
+                            title: `No results found`,
+                            content: 'Click the button above to add an expense.',
+                        }"
+                        :paginate="{ options: [15], selected: 15, current: 1 }"
+                        :items="budget.data.expenses[selectedItem]"
+                        @column-event="handleColumnEvent($event)"
+                    />
+                </div>
+            </section>
         </AuthenticatedContentLayout>
     </AuthenticatedLayout>
 </template>
