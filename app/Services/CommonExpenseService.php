b7bf090ac0a15f3033c61a1ac7996ad424ff0818
Justin Navarro <info@justinnavarro.net>
2024-11-12 22:35:15 -0500

    create saveExpenses and getBudgetRelationship methods in the common expense service

--- app/Services/CommonExpenseService.php
+++ app/Services/CommonExpenseService.php
@@ -2,90 +2,128 @@
 
 namespace App\Services;
 
-use App\Http\Requests\CommonExpenseRequest;
+use App\Enums\ExpenseTypeEnum;
+use App\Models\Bank;
+use App\Models\BankTemplate;
+use App\Models\Budget;
+use App\Models\BudgetTemplate;
+use App\Models\Childcare;
 use App\Models\ChildcareTemplate;
+use App\Models\CreditCard;
+use App\Models\CreditCardTemplate;
+use App\Models\Education;
 use App\Models\EducationTemplate;
+use App\Models\Entertainment;
 use App\Models\EntertainmentTemplate;
+use App\Models\Food;
 use App\Models\FoodTemplate;
+use App\Models\Gift;
 use App\Models\GiftTemplate;
+use App\Models\Housing;
 use App\Models\HousingTemplate;
+use App\Models\Loan;
 use App\Models\LoanTemplate;
+use App\Models\Medical;
 use App\Models\MedicalTemplate;
+use App\Models\Miscellaneous;
 use App\Models\MiscellaneousTemplate;
+use App\Models\Personal;
 use App\Models\PersonalTemplate;
+use App\Models\Shopping;
 use App\Models\ShoppingTemplate;
+use App\Models\Subscription;
 use App\Models\SubscriptionTemplate;
+use App\Models\Tax;
 use App\Models\TaxTemplate;
+use App\Models\Travel;
 use App\Models\TravelTemplate;
+use App\Models\Utility;
 use App\Models\UtilityTemplate;
+use Carbon\Carbon;
+use Exception;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Symfony\Component\HttpFoundation\Response;
+use Throwable;
 
 class CommonExpenseService
 {
     protected $models = [
+        'bank' => [
+            'budget' => Bank::class,
+            'template' => BankTemplate::class,
+        ],
         'childcare' => [
-            'budget' => '',
+            'budget' => Childcare::class,
             'template' => ChildcareTemplate::class,
         ],
+        'creditCard' => [
+            'budget' => CreditCard::class,
+            'template' => CreditCardTemplate::class,
+        ],
         'education' => [
-            'budget' => '',
+            'budget' => Education::class,
             'template' => EducationTemplate::class,
         ],
         'entertainment' => [
-            'budget' => '',
+            'budget' => Entertainment::class,
             'template' => EntertainmentTemplate::class,
         ],
         'food' => [
-            'budget' => '',
+            'budget' => Food::class,
             'template' => FoodTemplate::class,
         ],
-        'gifts' => [
-            'budget' => '',
+        'gift' => [
+            'budget' => Gift::class,
             'template' => GiftTemplate::class,
         ],
         'housing' => [
-            'budget' => '',
+            'budget' => Housing::class,
             'template' => HousingTemplate::class,
         ],
+        'income' => [],
+        'investment' => [],
         'loan' => [
-            'budget' => '',
+            'budget' => Loan::class,
             'template' => LoanTemplate::class,
         ],
         'medical' => [
-            'budget' => '',
+            'budget' => Medical::class,
             'template' => MedicalTemplate::class,
         ],
         'miscellaneous' => [
-            'budget' => '',
+            'budget' => Miscellaneous::class,
             'template' => MiscellaneousTemplate::class,
         ],
         'personal' => [
-            'budget' => '',
+            'budget' => Personal::class,
             'template' => PersonalTemplate::class,
         ],
         'shopping' => [
-            'budget' => '',
+            'budget' => Shopping::class,
             'template' => ShoppingTemplate::class,
         ],
         'subscription' => [
-            'budget' => '',
+            'budget' => Subscription::class,
             'template' => SubscriptionTemplate::class,
         ],
         'tax' => [
-            'budget' => '',
+            'budget' => Tax::class,
             'template' => TaxTemplate::class,
         ],
         'travel' => [
-            'budget' => '',
+            'budget' => Travel::class,
             'template' => TravelTemplate::class,
         ],
         'utility' => [
-            'budget' => '',
+            'budget' => Utility::class,
             'template' => UtilityTemplate::class,
         ],
+        'vehicle' => [],
     ];
 
-    public function getModel(CommonExpenseRequest $request, bool $template)
+    public function getModel(Request $request, bool $template)
     {
         $key = $this->getKey($request);
 
@@ -96,7 +134,55 @@ class CommonExpenseService
         return $model;
     }
 
-    private function getKey(CommonExpenseRequest $request)
+    public function saveExpenses(BudgetTemplate $template, array $validated): Budget
+    {
+        try {
+            DB::beginTransaction();
+
+            $budget = auth()->user()->budgets()->create([
+                'name' => '',
+                'budget_cycle' => Carbon::create((int) $validated['year'], (int) $validated['month']),
+            ]);
+
+            foreach (ExpenseTypeEnum::allExpenses() as $expense) {
+                if (!method_exists($template, $expense)) {
+                    continue;
+                }
+
+                foreach ($template->{$expense} as $data) {
+                    if (!method_exists($budget, $expense)) {
+                        continue;
+                    }
+
+                    Log::info("Expense: {$expense}, Data: {$data}");
+                    $budget->{$expense}()->create([
+                        'data' => $data->data,
+                        'expense_type_id' => $data->expense_type_id,
+                        'user_vehicle_id' => $data->user_vehicle_id ?? null,
+                    ]);
+                }
+            }
+
+            DB::commit();
+            return $budget;
+        } catch (Throwable $e) {
+            DB::rollBack();
+//            Log::debug("Expense: {$expense}, Data: {$data}");
+            throw new Exception($e->getMessage());
+        }
+    }
+
+    public function getBudgetRelationship(Request $request, bool $template): array
+    {
+        if (! $template && $uuid = extractUuid($request->header('referer'))) {
+            $budget = Budget::where('uuid', $uuid)->firstOrFail();
+            return ['budget_id' => $budget->id];
+        }
+
+        return ['budget_template_id' => auth()->user()->budgetTemplate->id];
+    }
+
+    private function getKey(Request $request)
     {
         $list = explode('/', $request->path());
         $count = count($list);
