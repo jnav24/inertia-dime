60f1d3d62d7461f96783c55480e27c3b2b3d6df5
Justin Navarro <info@justinnavarro.net>
2024-11-13 17:46:47 -0500

    added traits to bank models

--- app/Models/Bank.php
+++ app/Models/Bank.php
@@ -4,13 +4,14 @@ namespace App\Models;
 
 use App\Casts\EncryptedExpenseGain;
 use App\Traits\HasUuids;
+use App\Traits\WithBudget;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Bank extends Model
 {
-    use SoftDeletes, HasUuids;
+    use SoftDeletes, HasUuids, WithBudget;
 
     protected $fillable = [
         'uuid',
