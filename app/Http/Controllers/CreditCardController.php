9dd9d386fb68842a7a07e421766de4b06d4c344d
Justin Navarro <info@justinnavarro.net>
2024-11-16 10:54:26 -0500

    update the credit card controller to accept template and non-template get and patch requests

--- app/Http/Controllers/CreditCardController.php
+++ app/Http/Controllers/CreditCardController.php
@@ -5,62 +5,62 @@ namespace App\Http\Controllers;
 use App\Data\CreditCardDto;
 use App\Http\Requests\CreditCardRequest;
 use App\Models\CreditCardTemplate;
+use App\Services\CommonExpenseService;
 
 class CreditCardController extends Controller
 {
+    public function __construct(protected CommonExpenseService $commonExpenseService)
+    {}
+
     public function store(CreditCardRequest $request)
     {
         $validated = $request->validated();
 
-        if ($validated['template']) {
-            CreditCardTemplate::create([
-                'data' => new CreditCardDto(
-                    name: $validated['name'],
-                    amount: $validated['amount'],
-                    due_date: $validated['due_date'],
-                    apr: $validated['apr'] ?? null,
-                    balance: $validated['balance'] ?? null,
-                    exp_month: $validated['exp_month'] ?? null,
-                    exp_year: $validated['exp_year'] ?? null,
-                    last_4: $validated['last_4'] ?? null,
-                    limit: $validated['limit'] ?? null,
-                ),
-                'expense_type_id' => $validated['account_type'],
-                'budget_template_id' => auth()->user()->budgetTemplate->id,
-            ]);
+        $this->commonExpenseService->getModel($request, $validated['template'])::create([
+            'data' => new CreditCardDto(
+                name: $validated['name'],
+                amount: $validated['amount'],
+                due_date: $validated['due_date'],
+                apr: $validated['apr'] ?? null,
+                balance: $validated['balance'] ?? null,
+                exp_month: $validated['exp_month'] ?? null,
+                exp_year: $validated['exp_year'] ?? null,
+                last_4: $validated['last_4'] ?? null,
+                limit: $validated['limit'] ?? null,
+            ),
+            'expense_type_id' => $validated['account_type'],
+            ...$this->commonExpenseService->getBudgetRelationship($request, $validated['template']),
+        ]);
 
-            return redirect()->route('budget.template.index')
-                ->with('message', 'Credit card was saved successfully');
-        }
-
-        return redirect()->back();
+        return redirect()->back()
+            ->with('message', $validated['name'] . ' was created successfully');
     }
 
     public function update(CreditCardRequest $request, string $uuid)
     {
         $validated = $request->validated();
 
-        if ($validated['template']) {
-            $template = CreditCardTemplate::where('uuid', $uuid)->firstOrFail();
-            $template->update([
-                'data' => new CreditCardDto(
-                    name: $validated['name'],
-                    amount: $validated['amount'],
-                    due_date: $validated['due_date'],
-                    apr: $validated['apr'] ?? null,
-                    balance: $validated['balance'] ?? null,
-                    exp_month: $validated['exp_month'] ?? null,
-                    exp_year: $validated['exp_year'] ?? null,
-                    last_4: $validated['last_4'] ?? null,
-                    limit: $validated['limit'] ?? null,
-                ),
-                'expense_type_id' => $validated['account_type'],
-            ]);
+        $template = $this->commonExpenseService->getModel($request, $validated['template'])::query()
+            ->withBudget()
+            ->where('uuid', $uuid)
+            ->firstOrFail();
 
-            return redirect()->route('budget.template.index')
-                ->with('message', 'Credit card was updated successfully');
-        }
+        $template->update([
+            'data' => new CreditCardDto(
+                name: $validated['name'],
+                amount: $validated['amount'],
+                due_date: $validated['due_date'],
+                apr: $validated['apr'] ?? null,
+                balance: $validated['balance'] ?? null,
+                exp_month: $validated['exp_month'] ?? null,
+                exp_year: $validated['exp_year'] ?? null,
+                last_4: $validated['last_4'] ?? null,
+                limit: $validated['limit'] ?? null,
+            ),
+            'expense_type_id' => $validated['account_type'],
+        ]);
 
-        return redirect()->back();
+        return redirect()->back()
+            ->with('message', $validated['name'] . ' was updated successfully');
     }
 }
