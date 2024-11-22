7d8bb3517c1f35933cbaf34d14ebbd4308fb0493
Justin Navarro <info@justinnavarro.net>
2024-11-22 09:32:14 -0500

    update the budget aggregation to accept a budget to display aggregation separately from the budget

--- app/Http/Resources/BudgetAggregationResource.php
+++ app/Http/Resources/BudgetAggregationResource.php
@@ -2,19 +2,22 @@
 
 namespace App\Http\Resources;
 
-use App\Models\BudgetAggregation;
+use App\Models\Budget;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-/** @mixin BudgetAggregation */
+/** @mixin Budget */
 class BudgetAggregationResource extends JsonResource
 {
     public function toArray(Request $request): array
     {
         return [
-            'id' => $this->uuid,
-            'data' => $this->data,
-            'budget' => new BudgetResource($this->whenLoaded('budget')),
+            'id' => $this->whenLoaded('aggregation', $this->aggregation?->uuid),
+            'budget_cycle' => $this->budget_cycle,
+            'data' => $this->whenLoaded('aggregation', $this->aggregation?->data?->reduce(function ($result, $aggregation) {
+                $result[$aggregation->type->value] = $aggregation->value;
+                return $result;
+            }, [])),
         ];
     }
 }
