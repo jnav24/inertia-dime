7760de78fd7de2c838b0806ea60fd3d781169566
Justin Navarro <info@justinnavarro.net>
2024-11-27 17:12:02 -0500

    reformat the report results

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Resources\ExpenseTypeResource;
+use App\Http\Resources\ReportResource;
 use App\Http\Resources\TemplateResource;
 use App\Models\Budget;
 use App\Models\ExpenseType;
@@ -45,14 +46,22 @@ class ReportController extends Controller
                 ->where('user_id', auth()->user()->id)
                 ->where('budget_cycle', 'LIKE', $validated['year'] . '%')
                 ->get()
-                ->pluck($validated['expense'])
-                ->flatten();
+                ->map(function (Budget $budget) use ($validated) {
+                    return $budget->{$validated['expense']}->map(function ($ex) use ($budget) {
+                        $ex->budget_cycle = $budget->budget_cycle;
+                        return new ReportResource($ex);
+                    });
+                })
+                ->flatten()
+                ->groupBy(
+                    fn($report) => Carbon::parse($report["budget_cycle"])->format("M")
+                );
         }
 
         return Inertia::render('Report', [
             'aggregations' => $aggregations,
             'types' => ExpenseType::grouped()->map(fn ($object) => ExpenseTypeResource::collection($object)),
-            'results' => TemplateResource::collection($results),
+            'results' => $results,
         ]);
     }
 }
