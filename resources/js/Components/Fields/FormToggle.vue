a52c2bc926f5bbf3499d6b36d2466724aa64bed7
Justin Navarro <info@justinnavarro.net>
2024-11-28 17:29:48 -0500

    update the FormToggle to be a part of the FormContext

--- resources/js/Components/Fields/FormToggle.vue
+++ resources/js/Components/Fields/FormToggle.vue
@@ -1,39 +1,59 @@
 <script setup lang="ts">
+import useForm from '@/Composables/useForm';
+import type { RulesType } from '@/types/form';
+import { computed } from 'vue';
+
 type Emits = {
     (e: 'update:toggle', v: boolean): void;
     (e: 'setToggle', v: boolean): void;
 };
 
 type Props = {
-    disabled?: boolean;
+    label: string;
+    validateOnInit?: boolean;
+    rules?: RulesType | Array<keyof RulesType>;
     toggle: boolean;
 };
 
 const emit = defineEmits<Emits>();
 const props = defineProps<Props>();
 
+const { error, labelId, getInputValue, updateInputValue } = useForm({
+    label: props.label,
+    validateOnInit: props.validateOnInit,
+    value: props.toggle ? 'checked' : '',
+    rules: props.rules,
+});
+
+const isChecked = computed(() => getInputValue.value === 'checked');
+
 const handleUpdate = () => {
-    if (!props.disabled) {
-        emit('update:toggle', !props.toggle);
-        emit('setToggle', !props.toggle);
-    }
+    updateInputValue(isChecked.value ? '' : 'checked');
+    emit('update:toggle', isChecked.value);
+    emit('setToggle', isChecked.value);
 };
 </script>
 
 <template>
-    <button
-        @click="handleUpdate()"
-        class="w-16 rounded-full border text-left shadow-inner transition-all duration-100 ease-in-out focus:outline-none"
-        :class="{
-            'border-primary border-opacity-50 bg-primary bg-opacity-50': toggle,
-            'border-gray-200 bg-gray-200': !toggle,
-        }"
-        style="height: 2.15rem"
-        type="button"
-    >
-        <span
-            class="inline-block h-8 w-8 transform rounded-full bg-white transition duration-100 ease-in-out"
-            :class="{ 'translate-x-9/10': toggle, 'translate-x-0': !toggle }"
-        ></span>
-    </button>
+    <div>
+        <input type="hidden" :name="labelId" :id="labelId" :value="getInputValue" />
+        <button
+            @click="handleUpdate()"
+            class="w-16 rounded-full border text-left shadow-inner transition-all duration-100 ease-in-out focus:outline-none"
+            :class="{
+                'border-primary border-opacity-50 bg-primary bg-opacity-50': isChecked,
+                'border-gray-200 bg-gray-200': !isChecked,
+            }"
+            style="height: 2.15rem"
+            type="button"
+        >
+            <span
+                class="inline-block h-8 w-8 transform rounded-full bg-white transition duration-100 ease-in-out"
+                :class="{
+                    'translate-x-9/10': isChecked,
+                    'translate-x-0': !isChecked,
+                }"
+            ></span>
+        </button>
+    </div>
 </template>
