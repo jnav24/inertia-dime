62cc11c09fd50a5f8ae99b4a77f7ffd1bb8777f0
Justin Navarro <info@justinnavarro.net>
2024-12-18 09:40:00 -0500

    update the profile mfa section based on changes to the modal

--- resources/js/Components/settings/Mfa.vue
+++ resources/js/Components/settings/Mfa.vue
@@ -2,21 +2,50 @@
 import Typography from '@/Components/Elements/Typography.vue';
 import FormToggle from '@/Components/Fields/FormToggle.vue';
 import BudgetForm from '@/Components/Fields/BudgetForm.vue';
+import { MFASetup } from '@/types/mfa';
+import MfaModal from '@/Components/modals/MfaModal.vue';
+import { ref, watchEffect } from 'vue';
 
 type Props = {
     hasMfa: boolean;
+    mfa: null | MFASetup;
 };
 
-defineProps<Props>();
+const props = defineProps<Props>();
+
+const mfaData = ref<null | MFASetup>(null);
+
+const showModal = ref(false);
+
+watchEffect(() => {
+    if (props.mfa) {
+        mfaData.value = props.mfa;
+        showModal.value = true;
+    }
+});
+
+const reset = () => {
+    mfaData.value = null;
+    showModal.value = false;
+};
 </script>
 
 <template>
+    <MfaModal
+        :mfa="mfaData as unknown as MFASetup"
+        :show="showModal"
+        @handleUpdate="showModal = $event"
+        @reset="reset()"
+    />
+
     <div class="flex items-center justify-between">
         <div>
             <Typography tag="h2" variant="h3">Two Factor Authentication</Typography>
+
             <Typography variant="caption">
                 Add additional security to your account by using two factor authentication.
             </Typography>
+
             <Typography variant="caption">
                 You will need an authenticator app like Authy or Google Authenticator to use two
                 factor.
