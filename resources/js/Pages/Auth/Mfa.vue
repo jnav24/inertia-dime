8c901898ffe0544463d98a8f1abb4898e977997a
Justin Navarro <info@justinnavarro.net>
2024-12-22 17:52:30 -0500

    initial build of the mfa form on the login page

--- resources/js/Pages/Auth/Mfa.vue
+++ resources/js/Pages/Auth/Mfa.vue
@@ -1,9 +1,29 @@
 <script setup lang="ts">
 import GuestLayout from '@/Layouts/GuestLayout.vue';
+import BudgetForm from '@/Components/Fields/BudgetForm.vue';
+import FormMfa from '@/Components/Fields/FormMfa.vue';
+import { Head } from '@inertiajs/vue3';
+import FormButton from '@/Components/Fields/FormButton.vue';
+import Typography from '@/Components/Elements/Typography.vue';
 </script>
 
 <template>
     <GuestLayout>
-        <pre>hello motto</pre>
+        <Head title="Login" />
+
+        <div class="my-12 space-y-4">
+            <Typography variant="h1">Two-factor Authentication</Typography>
+            <Typography variant="body2">
+                Enter the code displayed in your authenticator app.
+            </Typography>
+        </div>
+
+        <BudgetForm>
+            <div class="mb-6">
+                <FormMfa label="Code" :rules="['required', 'numeric']" />
+            </div>
+
+            <FormButton block color="primary" submit>Verify account</FormButton>
+        </BudgetForm>
     </GuestLayout>
 </template>
