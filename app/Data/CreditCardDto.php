f27e3f6c3c67d960dd39fa7694182b05be950257
Justin Navarro <info@justinnavarro.net>
2024-12-28 13:47:59 -0500

    add missing properties to the CreditCardDto

--- app/Data/CreditCardDto.php
+++ app/Data/CreditCardDto.php
@@ -2,12 +2,17 @@
 
 namespace App\Data;
 
+use DateTime;
+
 class CreditCardDto extends ExpenseSpendDto
 {
     public function __construct(
         string $name,
         float $amount,
         int $due_date,
+        ?string $confirmation = null,
+        ?DateTime $paid_date = null,
+        ?string $notes = null,
         public ?float $apr = null,
         public ?float $balance = null,
         public ?int $exp_month = null,
@@ -16,6 +21,6 @@ class CreditCardDto extends ExpenseSpendDto
         public ?float $limit = null,
     )
     {
-        parent::__construct($name, $amount, $due_date);
+        parent::__construct($name, $amount, $due_date, $confirmation, $paid_date, $notes);
     }
 }
