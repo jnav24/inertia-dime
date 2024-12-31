9172461b1369a3c6d565d5be1b70ad1699f00b79
Justin Navarro <info@justinnavarro.net>
2024-12-31 09:29:43 -0500

    add root username to the database config and env

--- config/database.php
+++ config/database.php
@@ -49,6 +49,7 @@ return [
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'laravel'),
             'backup_database' => env('DB_BACKUP_DATABASE', 'laravel'),
+            'root_username' => env('DB_ROOT_USERNAME', 'root'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
