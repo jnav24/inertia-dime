ec66b6b414629b8d3ab67e734fe7ae7e26536f2a
Justin Navarro <info@justinnavarro.net>
2025-01-02 00:10:05 -0500

    created ColumnIsPaid component

---
+++ resources/js/Components/table/ColumnIsPaid.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import CheckCircle from '@/Components/Icons/solid/CheckCircle.vue';
+import { ValueOfExpense } from '@/types/expenses';
+
+defineProps<{ obj: ValueOfExpense }>();
+</script>
+
+<template>
+    <CheckCircle v-if="obj.data?.confirmation" classes="size-8 text-primary" />
+</template>
