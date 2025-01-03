b29b402632c8e060d38feeb096f0dadd6ac7a56f
Justin Navarro <info@justinnavarro.net>
2025-01-03 01:05:17 -0500

    add default values to confirmation and paid date fields

--- resources/js/Components/Forms/ExpenseFormConfirmation.vue
+++ resources/js/Components/Forms/ExpenseFormConfirmation.vue
@@ -3,6 +3,15 @@ import FormInput from '@/Components/Fields/FormInput.vue';
 import FormCheckbox from '@/Components/Fields/FormCheckbox.vue';
 import FormTextarea from '@/Components/Fields/FormTextarea.vue';
 import FormDatePicker from '@/Components/Fields/FormDatePicker.vue';
+import { ExpenseFormActionProps } from '@/types/expenses';
+
+type Props = {
+    expense: {
+        data: ExpenseFormActionProps;
+    };
+};
+
+defineProps<Props>();
 </script>
 
 <template>
@@ -10,8 +19,8 @@ import FormDatePicker from '@/Components/Fields/FormDatePicker.vue';
         <FormCheckbox label="Calculate this purchase in your totals?" />
 
         <div class="mt-4 grid grid-cols-2 gap-4">
-            <FormInput label="Confirmation" />
-            <FormDatePicker label="Paid Date" />
+            <FormInput label="Confirmation" :value="expense?.data.confirmation" />
+            <FormDatePicker label="Paid Date" :value="expense?.data.paid_date" />
         </div>
 
         <FormTextarea label="Notes" />
