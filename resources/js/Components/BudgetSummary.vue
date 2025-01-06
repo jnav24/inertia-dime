b710c9eceb18790afa5a7a64f4633a4e1126562c
Justin Navarro <info@justinnavarro.net>
2025-01-06 10:24:29 -0500

    created budget total and summary components

---
+++ resources/js/Components/BudgetSummary.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import BudgetTotal from '@/Components/BudgetTotal.vue';
+import Cash from '@/Components/Icons/outline/Cash.vue';
+import ShoppingBag from '@/Components/Icons/outline/ShoppingBag.vue';
+import Library from '@/Components/Icons/outline/Library.vue';
+import { Aggregation } from '@/types/budget';
+import { convertToCurrency } from '@/utils/functions';
+
+type Props = {
+    aggregation: Aggregation;
+};
+
+defineProps<Props>();
+</script>
+
+<template>
+    <section class="flex items-center justify-between">
+        <BudgetTotal
+            :icon="Cash"
+            title="Total Income"
+            :amount="convertToCurrency(aggregation.data.earned)"
+        />
+        <BudgetTotal
+            :icon="ShoppingBag"
+            title="Total Spent"
+            :amount="convertToCurrency(aggregation.data.spent)"
+        />
+        <BudgetTotal
+            :icon="Library"
+            title="Total Saved"
+            :amount="convertToCurrency(aggregation.data.saved)"
+        />
+    </section>
+</template>
---
+++ resources/js/Components/BudgetTotal.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { type ConcreteComponent, resolveComponent } from 'vue';
+
+type Props = {
+    amount: string;
+    icon?: ConcreteComponent | string;
+    title: string;
+};
+
+const props = defineProps<Props>();
+
+const setIcon = () => {
+    if (typeof props.icon === 'string') {
+        return resolveComponent(props.icon);
+    }
+
+    return props.icon;
+};
+</script>
+
+<template>
+    <div class="flex flex-row items-center">
+        <div class="mr-2 rounded-full bg-primary p-2">
+            <component :is="setIcon()" class="size-6 text-white" />
+        </div>
+
+        <div class="space-y-0">
+            <p class="text-sm text-gray-500">{{ title }}</p>
+            <p class="font-body text-xl text-gray-700">{{ amount }}</p>
+        </div>
+    </div>
+</template>
