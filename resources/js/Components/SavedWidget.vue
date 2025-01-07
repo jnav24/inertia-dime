1f67239794ae5256b8374bf349987716711fc29e
Justin Navarro <info@justinnavarro.net>
2025-01-07 00:15:50 -0500

    update the widget based on if the user is able to save 20% or not in their budget

--- resources/js/Components/SavedWidget.vue
+++ resources/js/Components/SavedWidget.vue
@@ -3,27 +3,49 @@ import ArrowTopRightSquare from '@/Components/Icons/outline/ArrowTopRightSquare.
 import FormButton from '@/Components/Fields/FormButton.vue';
 import Typography from '@/Components/Elements/Typography.vue';
 import Library from '@/Components/Icons/outline/Library.vue';
+import { computed } from 'vue';
+
+type Props = {
+    savings: number;
+    total: number;
+};
+
+const props = defineProps<Props>();
+
+const MIN = 20;
+
+const percentage = computed(() => (props.savings / props.total) * 100);
 </script>
 
 <template>
     <div
-        class="relative overflow-hidden rounded-xl bg-gradient-to-r from-success to-dm-primary px-4 py-2"
+        class="relative overflow-hidden rounded-xl bg-gradient-to-r px-4 py-2"
+        :class="{
+            'from-success to-dm-primary': percentage >= MIN,
+            'from-danger to-dm-danger': percentage < MIN,
+        }"
     >
         <div class="absolute -top-12 right-12 rotate-6">
-            <Library classes="size-48 text-success/25" />
+            <Library
+                :classes="`size-48 ${percentage < MIN ? 'text-danger/25' : 'text-success/25'}`"
+            />
         </div>
 
         <div class="relative z-20 flex items-center justify-between">
             <div>
                 <Typography variant="h1" tag="h3">
-                    <span class="text-white">21%</span>
+                    <span class="text-white">{{ percentage.toFixed(2) }}%</span>
                 </Typography>
                 <Typography variant="body2">
                     <span class="text-gray-200">Total saved</span>
                 </Typography>
             </div>
 
-            <FormButton color="primary" :icon="ArrowTopRightSquare" icon-position="right">
+            <FormButton
+                :color="percentage < MIN ? 'danger' : 'primary'"
+                :icon="ArrowTopRightSquare"
+                icon-position="right"
+            >
                 More Info
             </FormButton>
         </div>
