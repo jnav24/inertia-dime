c0be93540406bfe39ce4ed9781168de6f632aa2e
Justin Navarro <info@justinnavarro.net>
2025-01-07 00:14:11 -0500

    created blank modal to display expense overview information

---
+++ resources/js/Components/modals/SavingsModal.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import Modal from '@/Components/modals/Modal.vue';
+
+type Emits = {
+    (e: 'update:show', v: boolean): void;
+};
+
+type Props = {
+    show: boolean;
+};
+
+const emit = defineEmits<Emits>();
+defineProps<Props>();
+
+const closeModal = () => {
+    emit('update:show', false);
+};
+</script>
+
+<template>
+    <Modal :show="show" @close-modal="closeModal()" hide-close-button persistent>
+        <div class="w-200 space-y-4 p-4">Savings information here</div>
+    </Modal>
+</template>
