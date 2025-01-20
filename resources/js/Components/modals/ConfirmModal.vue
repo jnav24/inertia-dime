9a579b6d10f4f2b856630c576dff05d4d16e3f36
Justin Navarro <info@justinnavarro.net>
2025-01-20 11:21:26 -0500

    created confirm modal to delete items

--- resources/js/Components/modals/ConfirmModal.vue
+++ resources/js/Components/modals/ConfirmModal.vue
@@ -3,15 +3,18 @@ import Modal from '@/Components/modals/Modal.vue';
 import Typography from '@/Components/Elements/Typography.vue';
 import FormButton from '@/Components/Fields/FormButton.vue';
 import ExclamationTriangle from '@/Components/Icons/outline/ExclamationTriangle.vue';
+import BudgetForm from '@/Components/Fields/BudgetForm.vue';
+import FormInput from '@/Components/Fields/FormInput.vue';
+import { usePage } from '@inertiajs/vue3';
 
 type Emits = {
-    (e: 'confirm-action'): void;
-    (e: 'update:show', v: boolean): void;
+    (e: 'close-modal'): void;
 };
 
 type Props = {
     description?: string;
-    show: boolean;
+    id: string | null;
+    expense: string;
     title?: string;
 };
 
@@ -21,14 +24,38 @@ withDefaults(defineProps<Props>(), {
     title: 'Delete Item',
 });
 
-const confirmAction = () => {
-    emit('confirm-action');
-    emit('update:show', false);
+const page = usePage();
+const isTemplate = page.url.includes('template');
+
+const routes: Record<string, string> = {
+    banks: route('expense.bank.destroy'),
+    childcares: route('expense.childcare.destroy'),
+    'credit cards': route('expense.credit_cards.destroy'),
+    education: route('expense.education.destroy'),
+    entertainments: route('expense.entertainment.destroy'),
+    food: route('expense.food.destroy'),
+    gifts: route('expense.gift.destroy'),
+    housings: route('expense.housing.destroy'),
+    incomes: route('expense.income.destroy'),
+    investments: route('expense.investment.destroy'),
+    loans: route('expense.loan.destroy'),
+    medicals: route('expense.medical.destroy'),
+    miscellaneouses: route('expense.miscellaneous.destroy'),
+    personals: route('expense.personal.destroy'),
+    shoppings: route('expense.shopping.destroy'),
+    subscriptions: route('expense.subscription.destroy'),
+    taxes: route('expense.tax.destroy'),
+    travel: route('expense.travel.destroy'),
+    utilities: route('expense.utility.destroy'),
+    'user vehicles': route('user-vehicle.destroy'),
+    vehicles: route('expense.vehicle.destroy'),
 };
+
+const closeModal = () => emit('close-modal');
 </script>
 
 <template>
-    <Modal :show="show" @close-modal="$emit('update:show', false)" hide-close-button>
+    <Modal :show="!!id" @close-modal="closeModal()" hide-close-button>
         <div class="w-100 p-4">
             <div class="mb-6 flex space-x-4">
                 <div>
@@ -44,10 +71,14 @@ const confirmAction = () => {
                 </div>
             </div>
 
-            <div class="flex justify-end space-x-2">
-                <FormButton @onclick="$emit('update:show', false)">Cancel</FormButton>
-                <FormButton color="danger" @onclick="confirmAction()">Confirm</FormButton>
-            </div>
+            <BudgetForm @after-submit="closeModal()" :action="routes[expense]" method="delete">
+                <div class="flex justify-end space-x-2">
+                    <FormInput label="id" hidden :value="id ?? ''" />
+                    <FormInput label="template" hidden :value="isTemplate" />
+                    <FormButton @onclick="closeModal()">Cancel</FormButton>
+                    <FormButton color="danger" submit>Confirm</FormButton>
+                </div>
+            </BudgetForm>
         </div>
     </Modal>
 </template>
